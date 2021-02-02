f25935306bc83a09f1d81d703b89c23d6a62596d
Gershon-A <gershon.webgate@gmail.com>
2021-02-02 22:55:15 +0200

    Fxing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #################################
 
 SERVER=us1.ethermine.org:5555
-WALLET=0x1Fa418c70C5f14b21D00c242Bf369A875F129d12
+ETH_ADDRESS=0x1Fa418c70C5f14b21D00c242Bf369A875F129d12
 WORKER_NAME=$HOSTNAME
 
 #################################
@@ -13,5 +13,5 @@ WORKER_NAME=$HOSTNAME
 
 cd "$(dirname "$0")"
 
-/root/t-rex -a ethash -o stratum+tcp://$SERVER -u $WALLET-p x -w $WORKER_NAME 
+/root/t-rex -a ethash -o stratum+tcp://$SERVER -u $ETH_ADDRESS-p x -w $WORKER_NAME 
 
