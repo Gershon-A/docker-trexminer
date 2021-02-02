7b273b36d44d295fa417965b5cd4634765d7d4ce
Gershon-A <gershon.webgate@gmail.com>
2021-02-02 22:21:18 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #################################
 
 SERVER=us1.ethermine.org:5555
-ETH_ADDRESS=0x1Fa418c70C5f14b21D00c242Bf369A875F129d12
+WALLET=0x1Fa418c70C5f14b21D00c242Bf369A875F129d12
 WORKER_NAME=$HOSTNAME
 
 #################################
